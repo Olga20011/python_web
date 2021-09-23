95e393c1e7619b354d1de77b25a84c8d0ab34d53
olga20011 <olgaestherakello@gmail.com>
2021-09-23 14:45:49 +0300

    changes

--- school_system/settings.py
+++ school_system/settings.py
@@ -84,6 +84,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
